e81656dcd7b03adfe6914f3b8b3a33cb12777bd9
oleglobor <olegromanovichlviv@gmail.com>
2021-07-12 21:07:31 +0300

    homework

--- src/JavaMyTest/Study.java
+++ src/JavaMyTest/Study.java
@@ -2,6 +2,6 @@ package JavaMyTest;
 
 public class Study {
     public static void main(String[] args) {
-
+        System.out.println("Мої вітання, ви це бачите і зайлив його в гітхаб.Уря");
     }
 }
